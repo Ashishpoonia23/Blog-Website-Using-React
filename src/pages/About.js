c3031972062e2b7852f92d9405e6cc22b7aedb57
Ashishpoonia23 <107300327+Ashishpoonia23@users.noreply.github.com>
2022-12-22 21:36:52 +0530

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { MDBContainer, MDBTypography } from "mdb-react-ui-kit";
+
+const About = () => {
+  return (
+    <div style={{ marginTop: "100px" }}>
+     <h1>This Website is Made By Ashish Kumar Poonia</h1>
+     <div>
+    <p><b>Email:-</b> IT.1802556@gmail.com</p></div>
+    </div>
+  );
+};
+
+export default About;
